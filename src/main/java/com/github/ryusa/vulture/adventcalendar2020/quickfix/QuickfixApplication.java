59bf3f3cbcb858a3d9fd6e6a06da93e398a8677a
RyuSA <kommsussertad@gmail.com>
2020-10-27 01:26:26 +0000

    init

---
+++ src/main/java/com/github/ryusa/vulture/adventcalendar2020/quickfix/QuickfixApplication.java
@@ -0,0 +1,13 @@
+package com.github.ryusa.vulture.adventcalendar2020.quickfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickfixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ryusa/vulture/adventcalendar2020/quickfix/QuickfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ryusa.vulture.adventcalendar2020.quickfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
